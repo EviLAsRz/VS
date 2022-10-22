9a0a60250cc4da6e651ca250ec63c71614436960
EviLAsRz <alvarosantosrom9@gmail.com>
2022-10-22 21:20:16 +0200

    :sparkles:Acceso a django school desde maquina host completado

--- practica1/app/web_project/django_school/settings.py
+++ practica1/app/web_project/django_school/settings.py
@@ -83,17 +83,17 @@ WSGI_APPLICATION = 'django_school.wsgi.application'
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
 #DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    # }
-#    'default': {
-#        'ENGINE': 'django.db.backends.mysql',
-#        'OPTIONS': {
-            #'read_default_file': 'django_school/mariadb.cnf',
-            #'read_default_file': 'django_school/mariadb.cnf',
-#        },
-#    }
+     #'default': {
+     #    'ENGINE': 'django.db.backends.sqlite3',
+     #    'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+     #}
+    #'default': {
+    #    'ENGINE': 'django.db.backends.mysql',
+    #    'OPTIONS': {
+    #        #'read_default_file': 'django_school/mariadb.cnf',
+    #        'read_default_file': 'web_project/django_school/mariadb.cnf',
+    #    },
+    #}
 #}
 
 DATABASES = {
